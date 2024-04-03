bc49e0a31030c642c17c98036bce98ba6c097762
Anatoliy91 <dubinaanatolii@gmail.com>
2024-04-03 20:14:02 +0200

    tasks for hw4

---
+++ swap_variables.js
@@ -0,0 +1,20 @@
+let a = 10;
+let b = 100;
+let c=1;
+let d=1;
+c=a;
+d=b;
+b=c;
+a=d;
+console.log(a); 
+console.log(b);
+
+
+
+/*знайшо ось таке рішення задачі, але не дуже розумію як це працює, тому написав своє яке вище
+let a = 10;
+let b = 100;
+[a, b] = [b, a];
+console.log(a); 
+console.log(b);
+*/
