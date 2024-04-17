51b34f21c897c551ef0b1356ec8977140a309932
Anatoliy91 <dubinaanatolii@gmail.com>
2024-04-17 22:10:57 +0200

    dz7

---
+++ dz7_5.js
@@ -0,0 +1,11 @@
+//5. Створити функцію sum яка сумує 2 числа. Функція має викликатися наступним чином:
+//const result = sum(2)(3);
+// result == 5
+
+
+const sum = (a) => (b) => a+b;
+
+result = sum(2)(3);
+
+
+console.log(result)
